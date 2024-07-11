2db01b6b45263af4fa5d7829379731c27772b639
unknown <lazycode686@gmail.com>
2024-07-12 00:08:48 +0100

    cors policy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express, { json } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from 'cors'
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/userMiddleware.js";
 import connectDB from "./config/db.js";
@@ -11,6 +12,9 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
