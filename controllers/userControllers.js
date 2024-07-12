fe9f344c59162eec1e3d2aaaf7eb21665baf9b96
unknown <lazycode686@gmail.com>
2024-07-12 23:53:32 +0100

    token error fix

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -8,13 +8,14 @@ const authUser = asyncHandler(async (req, res) => {
 	const user = await User.findOne({ email });
 
 	if (user && (await user.matchPassword(password))) {
-		generateToken(res, user._id);
+		const token = generateToken(res, user._id);
 		res.status(201).json({
 			_id: user._id,
 			username: user.username,
 			email: user.email,
 			address: user.address,
 			isAdmin: user.isAdmin,
+			token,
 		});
 	} else {
 		res.status(401);
@@ -40,13 +41,14 @@ const registerUser = asyncHandler(async (req, res) => {
 	});
 
 	if (user) {
-		generateToken(res, user._id);
+		const token = generateToken(res, user._id);
 		res.status(201).json({
 			_id: user._id,
 			username: user.username,
 			email: user.email,
 			address: user.address,
 			isAdmin: user.isAdmin,
+			token,
 		});
 	} else {
 		res.status(400);
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,12 +1,9 @@
-const generateToken = (res, userId) => {
-	const token = Jwt.sign({ userId }, process.env.JWT_SECRET, {
+import Jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+	return Jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "30d",
 	});
-
-	res.cookie("jwt", token, {
-		httpOnly: true,
-		secure: process.env.NODE_ENV !== "development",
-		sameSite: "strict",
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-	});
 };
+
+export default generateToken;
